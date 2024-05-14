0e1b0013a465d829ec1968f77b677b4ab2d89c7f
shubhamkalokhe8 <mail2shubhamkalokhe04@gmail.com>
2024-05-14 15:03:06 +0530

    SHUBHAM
    write one SOP statment

--- GitOprations/src/com/test/Test.java
+++ GitOprations/src/com/test/Test.java
@@ -4,5 +4,6 @@ public class Test {
 public static void main(String[] args) {
 	System.out.println("Git opeartions");
 	System.out.println(">>>>>>>>>>>>>>>>>");
+	System.out.println("Git first change");
 }
 }
