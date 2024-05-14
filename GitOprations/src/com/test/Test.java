d683da40c11533dc9bdee611b4495057d941e06a
Audumbar Kalokhe <audumbar.mescorp@gmail.com>
2024-05-14 15:05:55 +0530

    Change Git

--- GitOprations/src/com/test/Test.java
+++ GitOprations/src/com/test/Test.java
@@ -5,5 +5,6 @@ public static void main(String[] args) {
 	System.out.println("Git opeartions");
 	System.out.println(">>>>>>>>>>>>>>>>>");
 	System.out.println("Git first change");
+	System.out.println("Git Push");
 }
 }
